17fc22ebb42c07707f7ab971db9666f6bdb869c6
Bastien VAN DEN NESTE <16327@ecam.be>
2020-03-16 21:51:45 +0100

    movement added

--- entity/player.py
+++ entity/player.py
@@ -1,34 +1,57 @@
 import pygame
-
+import math
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         self.health = 6
         self.maxhealth = 6
-        self.velocity = 0
+        self.velocity = [0,0]
         self.maxvelocity = 15
         self.damages = 10
         self.hitbox = pygame.Rect(10,10,50,50)
 
 
-    def position(self,axe):
+    def position(self):
+        if self.get_speed() >= self.maxvelocity:
+            coef = self.get_speed()/self.maxvelocity
+        else:
+            coef = 1
+        self.hitbox.x += self.velocity[0]/coef
+        self.hitbox.y += self.velocity[1]/coef
 
-        self.hitbox.x += self.velocity
 
-    def move_right(self):
+    def get_speed(self):
 
-        if self.velocity < self.maxvelocity:
-            self.velocity += 1
-            self.position("x")
-        elif self.velocity == self.maxvelocity:
-            self.position("x")
-            print("vmax")
+        return math.sqrt((self.velocity[0]**2)+(self.velocity[1]**2))
 
-    def move_left(self):
-        self.hitbox.x -= self.velocity
+    def move_right(self,state):
+        if state:
+            if self.velocity[0] < self.maxvelocity:
+                self.velocity[0] += 1
+        else:
+            if self.velocity[0] > 0:
+                self.velocity[0] -= 1
 
-    def move_up(self):
-        self.hitbox.y -= self.velocity
+    def move_left(self,state):
+        if state:
+            if self.velocity[0] > -self.maxvelocity:
+                self.velocity[0] -= 1
+        else:
+            if self.velocity[0] < 0:
+                self.velocity[0] += 1
 
-    def move_down(self):
-        self.hitbox.y += self.velocity
+    def move_up(self,state):
+        if state:
+            if self.velocity[1] > -self.maxvelocity:
+                self.velocity[1] -= 1
+        else:
+            if self.velocity[1] < 0:
+                self.velocity[1] += 1
+
+    def move_down(self,state):
+        if state:
+            if self.velocity[1] < self.maxvelocity:
+                self.velocity[1] += 1
+        else:
+            if self.velocity[1] > 0:
+                self.velocity[1] -= 1
