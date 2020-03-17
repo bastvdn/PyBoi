140c2a99c0033a12845a352252515d394b282b14
Bastien VAN DEN NESTE <16327@ecam.be>
2020-03-17 18:00:16 +0100

    shoot++

--- game/game.py
+++ game/game.py
@@ -1,6 +1,12 @@
 from entity import player
+import pygame
 
 class Game():
     def __init__(self):
         self.player = player.Player()
         self.pressed = {}
+
+    def showui(self):
+        heart = self.player.maxhealth
+        for n in range(heart/2):
+            print("ok")
